67b698a375a76952764dfd5b8a65aa252e241e57
Abhay Karade <abhaykarade9@gmail.com>
2022-11-11 16:24:20 -0500

    Add find_empty_space function,

--- scripts/kata_maze.py
+++ scripts/kata_maze.py
@@ -22,14 +22,32 @@ def read_maze(file_name):
         raise SystemExit
 
 
+def find_empty_space(maze):
+    row = 0
+    empty_spaces = []
+
+    for lst in maze:
+        empty = [(row, col) for col, val in enumerate(lst) if val == 0]
+        if empty:
+            empty_spaces.append(empty)
+        row = row+1
+
+    return empty_spaces
+
+
+
 
 if __name__ == "__main__":
 
-    maze_name = "maze5"
+    maze_name = "maze1"
     filepath = os.path.join("..", "mazes", f"{maze_name}.txt")
     print("filepath",filepath)
     maze = read_maze(filepath)
 
+
     #Print maze list as it looks in .txt file
     for row in maze:
         print(row, end="\n")
+
+    empty_spaces = find_empty_space(maze)
+    print("empty_spaces: ",empty_spaces)
