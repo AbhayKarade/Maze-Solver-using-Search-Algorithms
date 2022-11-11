e0b79db1c331f152272b0f15dcf38e1e0efc05f1
Abhay Karade <abhaykarade9@gmail.com>
2022-11-11 18:48:57 -0500

    display test cases

--- scripts/search.py
+++ scripts/search.py
@@ -45,45 +45,49 @@ class Search:
 if __name__ == "__main__":
 
     maze = [[0] * 3 for row in range(3)]
-    # for row in maze:
-    #     print(row)
+    print("**** sample Maze ****")
+    for row in maze:
+        print(row)
     start_pos = (0, 0)
     goal_pos = (2, 2)
     algo = Search(maze)
     result = algo.bfs(start_pos, goal_pos)
     assert result[0] == [(0, 0), (0, 1), (0, 2), (1, 2),
                          (2, 2)], "Assertion failed for Test 1"
-    print("**** Test1 passed *****")
+    print("**** Test1 passed ****")
 
     maze = read_maze(os.path.join("..", "mazes", "maze2.txt"))
-    # for row in maze:
-    #     print(row)
+    print("\n**** sample Maze2 ****")
+    for row in maze:
+        print(row)
     start_pos = (0, 4)
     goal_pos = (4, 4)
     algo = Search(maze)
     result = algo.bfs(start_pos, goal_pos)
     assert result[0] == [(0, 4), (1, 4), (2, 4), (3, 4),
                          (4, 4)], "Assertion failed for Test 2"
-    print("**** Test2 passed *****")
+    print("**** Test2 passed ****")
 
     maze = read_maze(os.path.join("..", "mazes", "maze2.txt"))
-    # for row in maze:
-    #     print(row)
+    print("\n**** sample Maze2 ****")
+    for row in maze:
+        print(row)
     start_pos = (0, 0)
     goal_pos = (3, 3)
     algo = Search(maze)
     result = algo.bfs(start_pos, goal_pos)
     assert result[0] is None, "Assertion failed for Test 3"
-    print("**** Test3 passed *****")
+    print("**** Test3 passed ****")
 
     maze = read_maze(os.path.join("..", "mazes", "maze3.txt"))
-    # for row in maze:
-    #     print(row)
+    print("\n**** sample Maze3 ****")
+    for row in maze:
+        print(row)
     start_pos = (0, 0)
     goal_pos = (3, 3)
     algo = Search(maze)
     result = algo.bfs(start_pos, goal_pos)
     assert result[0] == [(0, 0), (0, 1), (1, 1), (1, 2), (1, 3), (2, 3), (3, 3)], "Assertion failed for Test 4"
-    print("**** Test4 passed*****")
+    print("**** Test4 passed ****")
 
     print("\n**** All Test cases passed *****\n")
