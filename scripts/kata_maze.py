5bd7f6d599436d64c0f7c467e12b98c5a4e24c2f
Abhay Karade <abhaykarade9@gmail.com>
2022-11-11 18:49:33 -0500

    Add visualization and main function

--- scripts/kata_maze.py
+++ scripts/kata_maze.py
@@ -5,26 +5,73 @@ Email        : akarade@wpi.edu
 
 """
 import os
-from utils import read_maze, get_start_end_locations, offsets
+from utils import read_maze, get_start_end_locations
 
-def visulization():
-    pass
+from search import Search
+
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import numpy as np
+
+def visualization(start_pos, goal_pos, feed_maze, result, explored_nodes):
+    '''
+    This function uses Matplotlib to show plot of maze with final path
+
+    Following values will be assigned to variables for plotting 
+    empty spaces    : 0
+    obstacles       : 1
+    start_pos       : 2
+    final path      : 3
+    goal_pos        : 4
+
+    '''
+
+    coloring = {0: "white",
+                1: "black",
+                2: "red",
+                3: "green",
+                4: "blue"}
+
+    # Final Path
+    for i in result:
+        row, col = i
+        feed_maze[row][col] = 3
+
+    # Start Position
+    feed_maze[start_pos[0]][start_pos[1]] = 2
+
+    # Goal Position
+    feed_maze[goal_pos[0]][goal_pos[1]] = 4
+
+    unique_values = np.unique(feed_maze)
+    colormap = colors.ListedColormap([coloring[i] for i in unique_values])
+    plt.imshow(feed_maze, cmap=colormap)
+
+
+def main(maze):
+  
+    filepath = os.path.join("..", "mazes", f"{maze}.txt")
+
+    maze = read_maze(filepath)
+
+    # Get start and Goal location
+    _, start_pos, goal_pos = get_start_end_locations(maze)
+
+    algos = Search(maze)
+
+    result, explored_nodes = algos.bfs(start_pos[0], goal_pos[0])
+
+    visualization(start_pos[0], goal_pos[0],
+                        maze, result, explored_nodes)
+
+    plt.show()
+
+    print("\n**************Robot reached goal location******************")
+
 
 
 if __name__ == "__main__":
 
     maze_name = "maze2"
-    filepath = os.path.join("..", "mazes", f"{maze_name}.txt")
-    print("filepath",filepath)
-    maze = read_maze(filepath)
 
-
-    #Print maze list as it looks in .txt file
-    for row in maze:
-        print(row, end="\n")
-
-    empty_spaces, start_locations, exit_locations = get_start_end_locations(maze)
-
-    print("empty_spaces   : ",empty_spaces)
-    print("start_locations: ",start_locations)
-    print("exit_locations : ",exit_locations)
+    main(maze_name)
